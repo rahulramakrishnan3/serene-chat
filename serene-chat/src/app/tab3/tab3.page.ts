bba5d755259694b62368497e0d86abf9862c749a
Rahul KR <rahulramakrishnan512@gmail.com>
2025-02-01 17:38:49 +0530

    new services added

--- serene-chat/src/app/tab3/tab3.page.ts
+++ serene-chat/src/app/tab3/tab3.page.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonAvatar } from '@ionic/angular/standalone';
 import { GenericHeaderComponent } from "../components/generic-header/generic-header.component";
+import { ChatListService } from '../services/chat/chat-list.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, GenericHeaderComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, GenericHeaderComponent, IonList, IonItem, IonLabel, IonAvatar],
 })
 export class Tab3Page {
-  constructor() {}
+  chatList: any[] | undefined;
+  constructor(private chatListService: ChatListService) {}
+
   public header = {
     label: 'Chat list',
     backButton: true
+  }
+  
+  ngOnInit(){
+    this.chatListService.getContacts().subscribe(value => {
+      this.chatList = value;
+    })
   }
 }
